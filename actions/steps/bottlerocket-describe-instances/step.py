0bfa5bd56819bf73fd011f2905d8b885048c8def
Kenaz <kenaz@puppetlabs.com>
2020-06-12 21:27:58 -0700

    Adding STS support

--- actions/steps/bottlerocket-describe-instances/step.py
+++ actions/steps/bottlerocket-describe-instances/step.py
@@ -18,10 +18,17 @@ def instance_to_dict(ec2, instance):
 
 relay = Interface()
 
+session_token = None
+try:
+  session_token = relay.get(D.aws.connection.sessionToken)
+except:
+  pass
+
 sess = boto3.Session(
   aws_access_key_id=relay.get(D.aws.connection.accessKeyID),
   aws_secret_access_key=relay.get(D.aws.connection.secretAccessKey),
   region_name=relay.get(D.aws.region),
+  aws_session_token=session_token
 )
 ec2 = sess.resource('ec2')
 raw_instances = ec2.instances.all()
